e1f816f053ed8141d3f867b1463343328f99c714
kiwvic <torogao@protonmail.com>
2022-07-14 02:17:59 +0300

    move structures to internal.rs

---
+++ src/internal.rs
@@ -0,0 +1,35 @@
+use crate::*;
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Field {
+    pub field: Base64VecU8,
+}
+
+impl Field {
+    pub fn new(&mut self) -> Self {
+        Self {
+            field: Base64VecU8::from(vec![0u8; FIELD_LEN]),
+        }
+    }
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct GameInfo {
+    pub field: Field,
+    pub first_turn_time: Timestamp,
+    pub last_turn_time: Timestamp,
+}
+
+#[derive(BorshStorageKey, BorshSerialize)]
+pub struct PlayerStats {
+    pub wins: u32,
+    pub losses: u32,
+
+    pub fastest_game: u32,
+    pub longest_game: u32,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct TopPlayers {
+    pub list: Vector<AccountId>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,50 +6,13 @@ use near_sdk::{
     AccountId, Timestamp, PanicOnDefault
 };
 
+mod internal;
+
+use crate::internal::*;
 
 const FIELD_WIDTH: usize = 16;
 const FIELD_HEIGHT: usize = 16;
 const FIELD_LEN: usize = (FIELD_WIDTH / 8) * FIELD_HEIGHT;
-
-
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Field {
-    pub field: Base64VecU8,
-}
-
-
-impl Field {
-    pub fn new(&mut self) -> Self {
-        Self {
-            field: Base64VecU8::from(vec![0u8; FIELD_LEN]),
-        }
-    }
-}
-
-
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct GameInfo {
-    pub field: Field,
-    pub first_turn_time: Timestamp,
-    pub last_turn_time: Timestamp,
-}
-
-
-#[derive(BorshStorageKey, BorshSerialize)]
-pub struct PlayerStats {
-    pub wins: u32,
-    pub losses: u32,
-
-    pub fastest_game: u32,
-    pub longest_game: u32,
-}
-
-
-#[derive(BorshSerialize, BorshDeserialize)]
-pub struct TopPlayers {
-    pub list: Vector<AccountId>,
-}
-
 
 #[derive(BorshStorageKey, BorshSerialize)]
 pub enum StorageKeys {
